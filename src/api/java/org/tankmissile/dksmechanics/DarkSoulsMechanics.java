edb6ac0e87e91107bb26d45e9ee3140a6405b18e
TankMissile <trevans1@gmail.com>
2018-12-11 21:06:32 -0600

    initial commit

---
+++ src/api/java/org/tankmissile/dksmechanics/DarkSoulsMechanics.java
@@ -0,0 +1,10 @@
+package org.tankmissile.dksmechanics;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import org.dimdev.rift.listener.BlockAdder;
+import org.dimdev.rift.listener.ItemAdder;
+
+public class DarkSoulsMechanics implements BlockAdder, ItemAdder{
+
+}
